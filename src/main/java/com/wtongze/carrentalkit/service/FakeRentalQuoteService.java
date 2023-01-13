a4998525bbada38b6bf01e5d3b17e1ad99ca88a9
Tongze Wang <wtongze@gmail.com>
2023-01-13 01:32:33 -0800

    fix: move random to field

--- src/main/java/com/wtongze/carrentalkit/service/FakeRentalQuoteService.java
+++ src/main/java/com/wtongze/carrentalkit/service/FakeRentalQuoteService.java
@@ -15,7 +15,7 @@ import java.util.Random;
  */
 @Service("FakeRentalQuoteService")
 public class FakeRentalQuoteService implements RentalQuoteService {
-    private Random r;
+    private final Random r = new Random();
 
     @Override
     public Flux<RentalQuote> getStandardQuotes(String location, Timestamp start, Timestamp end, String promotionCode) {
@@ -24,7 +24,6 @@ public class FakeRentalQuoteService implements RentalQuoteService {
 
     @Override
     public Flux<RentalQuote> getOneWayQuotes(String pickUpLocation, String dropOffLocation, Timestamp start, Timestamp end, String promotionCode) {
-        Random r = new Random();
         List<RentalQuote> quotes = new ArrayList<>();
         int count = r.nextInt(1, 5);
         for (int i = 0; i < count; i++) {
@@ -34,12 +33,10 @@ public class FakeRentalQuoteService implements RentalQuoteService {
     }
 
     private Float randomPrice() {
-        Random r = new Random();
         return Math.round(r.nextFloat(20, 100) * 100) / 100f;
     }
 
     private CarType randomCarType() {
-        Random r = new Random();
         CarType[] carTypes = CarType.values();
         return carTypes[r.nextInt(carTypes.length)];
     }
